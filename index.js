8ef16df100f416d54c1bb048bf096f89320be74c
Mut1s0 <musyokaallan96@gmail.com>
2023-01-09 20:14:31 +0300

    used a for loop to log out leads

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
 
 inputBtn.addEventListener("click", function() {
-  console.log("Button clicked!")
+  myLeads.push(inputEl.value)
+  console.log(myLeads)
 })
+
+for ( let i = 0; i < myLeads.length; i++ ) {
+  console.log(myLeads[i])
+}
