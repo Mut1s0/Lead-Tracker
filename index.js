8783a1e7f1a94a8210c4f863f57b92d9319a560b
Mut1s0 <musyokaallan96@gmail.com>
2023-01-09 20:48:07 +0300

    create the render function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
-  console.log(myLeads)
+  //console.log(myLeads)
+  renderLeads()
 })
 
-for ( let i = 0; i < myLeads.length; i++ ) {
-  console.log(myLeads[i])
+function renderLeads() {
+  let listItems = ""
+  for ( let i = 0; i < myLeads.length; i++ ) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+  }
+  ulEl.innerHTML = listItems
 }
