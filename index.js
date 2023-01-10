2f40d8cf7e3a0f119c65cb4415470376ea9a81f5
Mut1s0 <musyokaallan96@gmail.com>
2023-01-10 11:34:46 +0300

    Get the current tab

--- index.js
+++ index.js
@@ -8,36 +8,42 @@ const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 let myLeads = []
 
-
+// STORE TO THE LOCAL STORAGE
 if ( leadsFromLocalStorage ) {
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
 
-const tabs = [
-  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
-]
 
+
+// SAVE THE CURRENT TAB WHEN CLICKED
 tabBtn.addEventListener("click", function() {
-  myLeads.push(tabs[0].url)
-  localStorage.setItem( 'myLeads', JSON.stringify( myLeads ) )
-  render(myLeads)
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem( 'myLeads', JSON.stringify( myLeads ) )
+    render(myLeads)
+  })
+
 })
 
 
+
+// RENDER FUNCTION
 function render(leads) {
   let listItems = ""
   for ( let i = 0; i < leads.length; i++ ) {
     listItems += `
-    <li>
-      <a href="${leads[i]}" target='_blank'> ${leads[i]} </a>
-    </li>
+      <li>
+        <a href="${leads[i]}" target='_blank'> ${leads[i]} </a>
+      </li>
     `
   }
   ulEl.innerHTML = listItems
 }
 
 
+
+// DELETE FROM THE INPUT WHEN DOUBLED CLICKED
 deleteBtn.addEventListener( "dblclick", function() {
   localStorage.clear()
   myLeads = []
@@ -45,6 +51,8 @@ deleteBtn.addEventListener( "dblclick", function() {
 })  
 
 
+
+// ADD INPUT FROM THE INPUT TAB
 inputBtn.addEventListener("click", function() {
   myLeads.push(inputEl.value)
   inputEl.value = ""
